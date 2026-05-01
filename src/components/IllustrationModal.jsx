ad5d55422c4ac2379084debb1072b05fd687bc2d
UWAK <raymond.rainsberger@gmail.com>
2026-04-30 17:22:47 -0700

    link fix

--- src/components/IllustrationModal.jsx
+++ src/components/IllustrationModal.jsx
@@ -1,22 +1,22 @@
 import { useEffect, useRef, useState } from 'react'
 
 const illustrations = [
-  { src: '/Wastelands.pdf', alt: 'Wastelands Issue 1' },
-  { src: '/Manifestation.pdf', alt: 'Contribution to Apollo City Comics Shadow Anthology' },
-  { src: '/page0016-17.jpg', alt: 'Yes, a Jojo reference' },
-  { src: '/GunborgSkirmisherFull.jpg', alt: 'Inaugural card art for FullAuto TCG' },
-  { src: '/WastelandLullaby.jpg', alt: 'Stars on the midnight ranges' },
-  { src: '/GoblinParty.jpg', alt: 'When you are tight with the neighbors downstairs' },
-  { src: '/OniFull.jpg', alt: 'Magitek worldbuilding exercise' },
-  { src: '/WitchBoltColors.jpg', alt: 'Future collaborations with DS artist Witch Bolt' },
-  { src: '/Wastelands2', alt: 'This will eventually come out...' },
-  { src: '/Lucid1.jpg', alt: 'In collaboration with MatteBlackStudios for the release of LUCID' },
-  { src: '/Lucid2.jpg', alt: 'In collaboration with MatteBlackStudios for the release of LUCID' },
-  { src: '/FrenetikaTheMage.jpg', alt: 'In collaboration with the release of the Frenetika PC game' },
-  { src: '/Friedrich.jpg', alt: 'I definitely do NOT have a favorite OC in the Wastelands series' },
-  { src: '/GallowanCommission.jpg', alt: 'The best fantasy race from the mind of the best Dungeon Master in the world' },
-  { src: '/Memoriam.jpg' },
-  { src: '/Sketch.jpg' },
+  { src: '/Illustrations/Wastelands.pdf', alt: 'Wastelands Issue 1' },
+  { src: '/Illustrations/Manifestation.pdf', alt: 'Contribution to Apollo City Comics Shadow Anthology' },
+  { src: '/Illustrations/page0016-17.jpg', alt: 'Yes, a Jojo reference' },
+  { src: '/Illustrations/GunborgSkirmisherFull.jpg', alt: 'Inaugural card art for FullAuto TCG' },
+  { src: '/Illustrations/WastelandLullaby.jpg', alt: 'Stars on the midnight ranges' },
+  { src: '/Illustrations/GoblinParty.jpg', alt: 'When you are tight with the neighbors downstairs' },
+  { src: '/Illustrations/OniFull.jpg', alt: 'Magitek worldbuilding exercise' },
+  { src: '/Illustrations/WitchBoltColors.jpg', alt: 'Future collaborations with DS artist Witch Bolt' },
+  { src: '/Illustrations/Wastelands2.jpg', alt: 'This will eventually come out...' },
+  { src: '/Illustrations/Lucid1.jpg', alt: 'In collaboration with MatteBlackStudios for the release of LUCID' },
+  { src: '/Illustrations/Lucid2.jpg', alt: 'In collaboration with MatteBlackStudios for the release of LUCID' },
+  { src: '/Illustrations/FrenetikaTheMage.jpg', alt: 'In collaboration with the release of the Frenetika PC game' },
+  { src: '/Illustrations/Friedrich.jpg', alt: 'I definitely do NOT have a favorite OC in the Wastelands series' },
+  { src: '/Illustrations/GallowanCommission.jpg', alt: 'The best fantasy race from the mind of the best Dungeon Master in the world' },
+  { src: '/Illustrations/Memoriam.jpg' },
+  { src: '/Illustrations/Sketch.jpg' },
 ]
 
 export default function IllustrationModal({ isOpen, onClose }) {
